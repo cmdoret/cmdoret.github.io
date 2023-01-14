8bc1a78df20d0588452a9c4cd89a33d2199be482
cmdoret <cyril.mattheydoret@gmail.com>
2023-01-14 22:08:27 +0100

    fix typos

--- search_facade.js
+++ search_facade.js
@@ -4,8 +4,8 @@ window.onload = function() {
         if (baseUrl.slice(-1) == "/") {
             baseUrl = baseUrl.slice(0, -1);
         }
-        var sha256='d399ad7a94a3eace104d9a8658fce33d401dac859fea8f9a2f250966473b6761';
-        var sha384='+Q6SmmPTDb/Jt7F6aHklMcZxnhUZlDihlWC13Ls635V7GOAjOcmc+xxxrtS4Ddyz';
+        var sha256='0b8f40b5ff5e96e3d33efddcea3e3812ba3442aeeca432accb70c19bf3259642';
+        var sha384='1aOV3Lgb1SsU1f8dMvoXYp3iSGD5yZzLME3AfzxDewm+al0T8eILxhFhTvydT18M';
         var loadSearch = document.createElement('script');
         loadSearch.src = baseUrl + '/search_bundle.min.js?h=' + sha256;
         loadSearch.setAttribute('integrity', 'sha384-' + sha384);
