44f76a20b170e74f996a1c839a48c5deccac395e
cmdoret <cyril.mattheydoret@gmail.com>
2023-01-15 01:46:26 +0100

    add missing import

--- search_facade.js
+++ search_facade.js
@@ -4,8 +4,8 @@ window.onload = function() {
         if (baseUrl.slice(-1) == "/") {
             baseUrl = baseUrl.slice(0, -1);
         }
-        var sha256='9d521edc272b0af22a652a02291cf68e897af54f0bb6c76627ec399033cb74cc';
-        var sha384='QgEZeTOYeIAV7BUZ4SRJDr4y8NbfVaCdSB6N70WFq2n8gKZXbTa/+qkTo4UdLBFK';
+        var sha256='82793d96733c2ff1c3607c04b7f8a4c648c0fd5394aa11a40a9eebf4f09ba5ee';
+        var sha384='pl9LyRt1oJc1ZZTUkWZ1UWqTVyyKXTGBJykMBSgrHkI7/KsenVtYOY6Fa/tAgwPd';
         var loadSearch = document.createElement('script');
         loadSearch.src = baseUrl + '/search_bundle.min.js?h=' + sha256;
         loadSearch.setAttribute('integrity', 'sha384-' + sha384);
