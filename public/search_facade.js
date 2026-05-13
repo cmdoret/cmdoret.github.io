3cf68f31e9c24b8e57188621c94ec769f52c96c9
cmdoret <cyril.mattheydoret@gmail.com>
2026-05-13 12:06:47 +0200

    chore: deploy website

--- public/search_facade.js
+++ public/search_facade.js
@@ -4,8 +4,8 @@ window.onload = function() {
         if (baseUrl.slice(-1) == "/") {
             baseUrl = baseUrl.slice(0, -1);
         }
-        var sha256='201ae3fb2d7c844f85100aa16e8507181515187564a0cd355810bc5898a1d481';
-        var sha384='jaik0n9J7saRYuniAQbva1GMi9kA1JOMHPliilevwm0+NlXiHwPCMIp5owfkCAV0';
+        var sha256='c992dbcb033f6ac1a1d569f411545efb57393e5426e4a4692cbc588de1bc257e';
+        var sha384='eEVL2yFpS19WyC6gUshsMe8904AFOFc0jToRX0X56pC/wt1NDLEPHU5agUfhXliF';
         var loadSearch = document.createElement('script');
         loadSearch.src = baseUrl + '/search_bundle.min.js?h=' + sha256;
         loadSearch.setAttribute('integrity', 'sha384-' + sha384);
